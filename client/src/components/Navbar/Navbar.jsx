d82b1cb63bf83a9e904ffe3a0f0c7a70f49367dd
Daniel <daniel.pe.15y15@gmail.com>
2020-02-08 17:27:34 -0300

    navbar-web

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -10,21 +10,26 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <img className="nav-image" src={Logotipo} alt="" />
+        <img className="nav-image" src={Logotipo} alt="" tabIndex="1" />
       </div>
       <ul className="navbar-menu">
-        <li>Quem somos?</li>
-        <li>Relatos</li>
-        <li>Contatos</li>
+        <li tabIndex="2">Quem somos?</li>
+        <li tabIndex="3">Relatos</li>
+        <li tabIndex="4">Contatos</li>
       </ul>
       <div className="navbar-social-network-container">
         <img className="svg-image" src={SocialNetworkSvg} alt="" />
+        <div className="svg-extends"> </div>
         <ul className="navbar-social-network">
           <li>
-            <img className="social-icon" src={Facebook} alt="Link para o facebook do vale pcd" />
+            <a href="" tabIndex="5">
+              <img className="social-icon" src={Facebook} alt="Link para o facebook do vale pcd" />
+            </a>
           </li>
           <li>
-            <img className="social-icon" src={Instagram} alt="Link para o instagram do vale pcd" />
+            <a href="" tabIndex="6">
+              <img className="social-icon" src={Instagram} alt="Link para o instagram do vale pcd" />
+            </a>
           </li>
         </ul>
       </div>
