3f8aeaaea8320ef9c508580a834f0b8835e3136a
Edgar Marques <edgarmarques.110@gmail.com>
2020-02-08 13:29:35 -0300

    Feature(project-base): Lint code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-    return (
-        <section className="app" />
-    );
+  return (
+    <section className="app" />
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const path = require('path');
 require('dotenv').config();
 
 keystone.init({
-  'name': 'Keystone ToDo List',
-  'static': [path.join(__dirname, './public')],
+  name: 'Keystone ToDo List',
+  static: [path.join(__dirname, './public')],
   'admin path': 'admin',
-  'port': process.env.PORT || 3001,
+  port: process.env.PORT || 3001,
   'auto update': true,
-  'mongo': 'mongodb://localhost/keystonetodo2',
-  'auth': true,
+  mongo: 'mongodb://localhost/keystonetodo2',
+  auth: true,
   'user model': 'User',
   'cookie secret': '6D61822FBEAED8635A4A52241FEC3',
 });
