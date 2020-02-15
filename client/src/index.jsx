026caa4399667e7dd52e948228f2cc9769b2a19e
Samuel Miranda <som3@cin.ufpe.br>
2020-02-15 18:47:29 -0300

    feature(contact): Inserting states and adding properties

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Navbar, AboutUs, Form, Info, Title } from './components';
 import './App.scss';
 
 function App() {
+  const [state, setState] = useState(false);
   return (
     <section className="app">
       <Navbar />
       <AboutUs />
       <Title title="Fale conosco!" />
       <div className="teste">
-        <Info />
-        <Form />
+        <Info onClickArrow={() => { setState(!state); }} className={state} />
+        <Form onClickArrow={() => { setState(!state); }} className={!state} />
       </div>
     </section>
   );
