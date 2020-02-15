83a8948120430b88a47196c47d2d69190b4487f5
Samuel Miranda <som3@cin.ufpe.br>
2020-02-15 09:33:54 -0300

    feature(contact): Inserting export files

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import Form from './Form';
+import Info from './Info';
+import InfoContainer from './InfoContainer';
+import Field from './Field';
+import Title from './Title';
+
+export {
+  Form,
+  Info,
+  InfoContainer,
+  Field,
+  Title,
+};
