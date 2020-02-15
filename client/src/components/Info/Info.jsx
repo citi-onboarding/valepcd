bc7ee71addca26908b7286be7a858f5b32760ba2
Samuel Miranda <som3@cin.ufpe.br>
2020-02-15 13:28:14 -0300

    feature(contact): Inserting acessibility tag and adjusted import

--- client/src/components/Info/Info.jsx
+++ client/src/components/Info/Info.jsx
@@ -4,14 +4,17 @@ import Phone from '../../Assets/call branco.svg';
 import Insta from '../../Assets/insta branco.svg';
 import Facebook from '../../Assets/facebook branco.svg';
 import Envelope from '../../Assets/envelope branco.svg';
+import './Info.scss';
 
 function Info() {
   return (
     <div className="info-container">
-      <InfoContainer src={Phone} text="(81) 3426-8978" alt="Icone Telefone" />
-      <InfoContainer src={Envelope} text="pcdvale@gmail.com" alt="Icone Email" />
-      <InfoContainer src={Facebook} text="/valePCD" alt="Icone Facebook" aref="https://www.facebook.com" />
-      <InfoContainer src={Insta} text="@pcdvale" alt="Icone Instagram" aref="https://www.instagram.com" />
+      <ul>
+        <InfoContainer src={Phone} text="(81) 3426-8978" alt="Icone Telefone" />
+        <InfoContainer src={Envelope} text="pcdvale@gmail.com" alt="Icone Email" />
+        <InfoContainer src={Facebook} text="/valePCD" alt="Icone Facebook" aref="https://www.facebook.com" />
+        <InfoContainer src={Insta} text="@pcdvale" alt="Icone Instagram" aref="https://www.instagram.com" />
+      </ul>
     </div>
   );
 }
