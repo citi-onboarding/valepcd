88122d7c6d0e58ee850e64f3a55a0c437b0862a2
Samuel Miranda <som3@cin.ufpe.br>
2020-02-15 09:30:37 -0300

    feature(contact): Updating butterfly position

--- client/src/components/Title/Title.jsx
+++ client/src/components/Title/Title.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Title.scss';
 import LeftWing from '../../Assets/left butterfly wing.svg';
 import RightWing from '../../Assets/right butterfly wing.svg';
 
 function Title({ title }) {
   return (
     <div className="title">
-      <img src={LeftWing} alt="" />
-      <h1>{title}</h1>
       <img src={RightWing} alt="" />
+      <h1>{title}</h1>
+      <img src={LeftWing} alt="" />
     </div>
   );
 }
