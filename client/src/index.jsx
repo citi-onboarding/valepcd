f41b7a2672c3ec5f5384db0d638b88d52a19122a
Pedro Reis <pgsr@cin.ufpe.br>
2020-02-15 17:11:18 -0300

    Feature

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Navbar } from './components/index';
+import { Navbar, AboutUs } from './components/index';
 
 function App() {
   return (
     <section className="app">
       <Navbar />
+      <AboutUs />
     </section>
   );
 }
