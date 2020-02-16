c14f4747cb9416e5dc8460bf96757d7f5f988d46
Samuel Miranda <som3@cin.ufpe.br>
2020-02-15 22:06:45 -0300

    feature(contact): Adding props and fixing ESLint erros

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Field from '../Field';
 import axiosPost from '../../global/func';
+import Arrow from '../../Assets/Backgrounds/right-chevron.svg';
 import './Form.scss';
 
-
-function Form() {
+function Form({ onClickArrow, className }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
@@ -12,24 +13,36 @@ function Form() {
 
   const handleSubmit = () => {
     axiosPost('/api/sendEmail', {
-      name: name,
-      email: email,
-      subject: subject,
-      text: text,
+      name,
+      email,
+      subject,
+      text,
     }, (res) => { console.log(res.data); });
   };
 
   return (
-    <div className="container-form">
+    <div className={`container-form ${className}`}>
       <form onSubmit={handleSubmit}>
-        <Field className='input'  value={name} label="Nome:" type="text" handleChange={setName} />
-        <Field className='input' value={email} label="E-mail:" type="email" handleChange={setEmail} />
-        <Field className='input' value={subject} label="Assunto:" type="text" handleChange={setSubject} />
-        <Field className='field-text' value={text} label="Digite aqui seu texto:" type="text" handleChange={setText} />
+        <Field className="input" value={name} label="Nome:" type="text" handleChange={setName} />
+        <Field className="input" value={email} label="E-mail:" type="email" handleChange={setEmail} />
+        <Field className="input" value={subject} label="Assunto:" type="text" handleChange={setSubject} />
+        <Field className="input field-text" value={text} label="Digite aqui seu texto:" type="text" handleChange={setText} />
         <button className="send-button" type="submit">Enviar</button>
       </form>
+      <button className="arrow-button" type="button" onClick={onClickArrow}>
+        <img src={Arrow} alt="" />
+      </button>
     </div>
   );
 }
 
+Form.propTypes = {
+  className: PropTypes.bool,
+  onClickArrow: PropTypes.func,
+};
+
+Form.defaultProps = {
+  className: false,
+  onClickArrow: () => {},
+};
 export default Form;
