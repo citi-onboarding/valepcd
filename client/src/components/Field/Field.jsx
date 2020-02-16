a538aa12d10c4c20a768cda0f7f75fe9b7200520
Samuel Miranda <som3@cin.ufpe.br>
2020-02-15 22:05:46 -0300

    feature(contact): Refactoring component field

--- client/src/components/Field/Field.jsx
+++ client/src/components/Field/Field.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Field.scss';
 
-function Field({ label, value, type, handleChange, className }) {
+function Field({
+  label, value, type, handleChange, className,
+}) {
   const handleInput = (event) => {
     handleChange(event.target.value);
   };
@@ -10,14 +12,16 @@ function Field({ label, value, type, handleChange, className }) {
     <div className="field">
       <label htmlFor={label}>
         <p>{label}</p>
-        <input value={value} type={type} id={label} onChange={handleInput} className={className} />
+        {className === 'input field-text'
+          ? <textarea value={value} type={type} id={label} onChange={handleInput} className={className} autoComplete="off" />
+          : <input value={value} type={type} id={label} onChange={handleInput} className={className} autoComplete="off" />}
       </label>
     </div>
   );
 }
 
 Field.propTypes = {
-  className:PropTypes.string,
+  className: PropTypes.string,
   value: PropTypes.string,
   label: PropTypes.string,
   type: PropTypes.string,
