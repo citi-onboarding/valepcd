2c840298e49ef19b243a1f1bc4401818cee46543
Samuel Miranda <som3@cin.ufpe.br>
2020-02-15 22:08:30 -0300

    feature(contact): Inserting new props and arrow

--- client/src/components/Info/Info.jsx
+++ client/src/components/Info/Info.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import InfoContainer from '../InfoContainer';
 import Phone from '../../Assets/call branco.svg';
 import Insta from '../../Assets/insta branco.svg';
 import Facebook from '../../Assets/facebook branco.svg';
 import Envelope from '../../Assets/envelope branco.svg';
+import Arrow from '../../Assets/Backgrounds/right-chevron.svg';
 import './Info.scss';
 
-function Info() {
+function Info({ onClickArrow, className }) {
   return (
-    <div className="info-container">
+    <div className={`info-container ${className}`}>
       <ul>
         <InfoContainer src={Phone} text="(81) 3426-8978" alt="Icone Telefone" />
         <InfoContainer src={Envelope} text="pcdvale@gmail.com" alt="Icone Email" />
         <InfoContainer src={Facebook} text="/valePCD" alt="Icone Facebook" aref="https://www.facebook.com" />
         <InfoContainer src={Insta} text="@pcdvale" alt="Icone Instagram" aref="https://www.instagram.com" />
       </ul>
+      <button className="arrow-button" type="button" onClick={onClickArrow}>
+        <img src={Arrow} alt="" />
+      </button>
     </div>
   );
 }
+
+Info.propTypes = {
+  className: PropTypes.bool,
+  onClickArrow: PropTypes.func,
+};
+
+Info.defaultProps = {
+  className: false,
+  onClickArrow: () => {},
+};
 
 export default Info;
