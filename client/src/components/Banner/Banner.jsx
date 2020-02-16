b7b25cd3d40f9bd8c0c99b336051aebe825e304e
argas7 <argas@cin.ufpe.br>
2020-02-16 11:38:05 -0300

    Feature(Banner): create banner component

---
+++ client/src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.scss';
+import { ReactComponent as BannerSVG } from '../../assets/banner.svg';
+
+function Banner() {
+  return (
+    <section id="banner">
+      <div className="banner-content">
+        <h1>Vale PCD</h1>
+        <p>Venha nos ajudar a transformar o</p>
+        <p>Vale PCD em realidade!</p>
+      </div>
+      <div className="banner-image">
+        <BannerSVG />
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
