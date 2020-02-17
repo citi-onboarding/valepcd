66af90efd2866555393f3f1f3e0174c60d378504
argas7 <argas@cin.ufpe.br>
2020-02-17 13:51:37 -0300

    Bugfix(contact): rebase index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Navbar, AboutUs } from './components/index';
+import { Navbar, AboutUs, Form, Info, Title } from './components';
 
 function App() {
   return (
     <section className="app">
       <Navbar />
       <AboutUs />
+      <Title title="Fale conosco!" />
+      <div className="teste">
+        <Form />
+        <Info />
+      </div>
     </section>
   );
 }
