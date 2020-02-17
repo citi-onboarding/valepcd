6295bb53bbb7dc83d0e293283d61d80fcea74845
argas7 <argas@cin.ufpe.br>
2020-02-17 13:54:33 -0300

    Bugfix(contact): rebase index.js (client)

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,19 @@
 import Navbar from './Navbar';
 import AboutUs from './AboutUs';
 import Banner from './Banner';
+import Form from './Form';
+import Info from './Info';
+import InfoContainer from './InfoContainer';
+import Field from './Field';
+import Title from './Title';
 
 export {
   Navbar,
   AboutUs,
   Banner,
+  Form,
+  Info,
+  InfoContainer,
+  Field,
+  Title,
 };
