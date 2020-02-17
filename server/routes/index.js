74819cee9aa2b4570ba05d8de093592116211afb
viniciuslima-99 <vrpl@cin.ufpe.br>
2020-02-17 13:26:44 -0300

    hotfix(contact): Fix the mailOptions

--- server/routes/index.js
+++ server/routes/index.js
@@ -46,10 +46,10 @@ module.exports = (app) => {
     })
 
     const mailOptions = {
-      from: `"${nome}" <${email}>`,
+      from: `"${name}" <${email}>`,
       // to: Put a email,
       subject: assunto,
-      text: `${nome} <${email}> <${telefone}>\n\n${mensagem}`
+      text: `${nome} <${email}>\n <${subject}> \n\n${mensage}`
     }
 
     transporter.sendMail(mailOptions, (error) => {
