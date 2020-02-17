470de0be47bbed217d74957bacc2a92f27fd3db9
viniciuslima-99 <vrpl@cin.ufpe.br>
2020-02-17 13:26:44 -0300

    hotfix(contact): Fix the mailOptions

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,6 @@
 const keystone = require('keystone');
 const path = require('path');
-<<<<<<< HEAD
-const cors = require('cors');
-=======
 const cors = require('cors')
->>>>>>> Bugfix(contact): rebase index.js (server)
 const nodemailer = require('nodemailer');
 
 const WhoWeAre = keystone.list('QuemSomos').model;
@@ -27,11 +23,6 @@ module.exports = (app) => {
   });
 
   // Send Mail
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> Bugfix(contact): rebase index.js (server)
 	app.post('/sendmail', (req, res) => {
     const {
       name,
@@ -41,11 +32,6 @@ module.exports = (app) => {
     } = JSON.stringify(req.body);
 
     // console.log(body)
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> Bugfix(contact): rebase index.js (server)
     const transporter = nodemailer.createTransport({
       service: "gmail",
       port: 587,
@@ -58,17 +44,10 @@ module.exports = (app) => {
     })
 
     const mailOptions = {
-<<<<<<< HEAD
-      from: `"${name}" <${email}>`,
-      // to: Put a email,
-      subject: assunto,
-      text: `${nome} <${email}>\n <${subject}> \n\n${mensage}`
-=======
       from: `"${nome}" <${email}>`,
       // to: Put a email,
       subject: assunto,
       text: `${nome} <${email}> <${telefone}>\n\n${mensagem}`
->>>>>>> Bugfix(contact): rebase index.js (server)
     }
 
     transporter.sendMail(mailOptions, (error) => {
