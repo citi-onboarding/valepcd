57b2c1d49448d25a5671dc1926be51ede3c263cd
Samuel Miranda <som3@cin.ufpe.br>
2020-02-18 20:43:31 -0300

    Feature(contact) Adjusted email body

--- server/routes/index.js
+++ server/routes/index.js
@@ -22,39 +22,39 @@ module.exports = (app) => {
     });
   });
 
-  // Send Mail
-	app.post('/api/sendmail', (req, res) => {
-    const {
-      name,
-      email,
-      subject,
-      text,
-    } = req.body;
+// Send Mail
+app.post('/api/sendmail', (req, res) => {
+  const {
+    name,
+    email,
+    subject,
+    text,
+  } = req.body;
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.TRANSPORTER_EMAIL,
-        pass: process.env.TRANSPORTER_PASSWORD,
-      },
-      tls: { rejectUnauthorized: false },
-    });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.TRANSPORTER_EMAIL,
+      pass: process.env.TRANSPORTER_PASSWORD,
+    },
+    tls: { rejectUnauthorized: false },
+  });
 
-    const mailOptions = {
-      from: `"${nome}" <${email}>`,
-      // to: Put a email,
-      subject: assunto,
-      text: `${nome} <${email}> <${telefone}>\n\n${mensagem}`
+  const mailOptions = {
+    from: `"${name}" <${email}>`,
+    to: 'valedopcd@gmail.com',
+    subject,
+    text: `\n ${subject} \n\n${text}`,
+  };
+
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send('Email enviado');
     }
-
-    transporter.sendMail(mailOptions, (error) => {
-      if (error) {
-        res.status(500).send(error);
-      } else {
-        res.status(200).send('Email enviado');
-      }
-    })
-	})
+  })
+})
 };
