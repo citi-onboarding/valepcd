4eff4b43853e2d7460f3dc0396604402f93c04a5
Daniel <daniel.pe.15y15@gmail.com>
2020-03-03 20:20:39 -0300

    tentativa 2

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ module.exports = (app) => {
   app.get('/static', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
+
+  app.get('static/js/main.c855601e.chunk.js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/static/js/main.c855601e.chunk.js'));
+  });
+
   app.get('static/js/main.c855601e.chunk.js', (req, res) => {
     res.sendFile(path.join(__dirname, './public/static/js/main.c855601e.chunk.js'));
   });
