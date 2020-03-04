3c6d8914c31f10d9ef33c0ee62a134257c1d0969
Daniel <daniel.pe.15y15@gmail.com>
2020-03-04 07:32:23 -0300

    f

--- client/src/global/func.js
+++ client/src/global/func.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-//const baseUrl = 'http://localhost:3001';
 const baseUrl = 'https://vale-pcd.herokuapp.com';
 
 const axiosGet = async (relativeUrl = '/', setState = () => { }) => {
