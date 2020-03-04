3e518f88f64d4f183af963174f09cc8f2798e7ed
Daniel <daniel.pe.15y15@gmail.com>
2020-03-03 21:03:07 -0300

    fist commit

---
+++ updates/0.0.1-admin.js
@@ -0,0 +1,12 @@
+const keystone = require('keystone');
+
+const User = keystone.list('User');
+
+module.exports = (done) => {
+  new User.model({
+    name: { first: 'admin', last: 'user' },
+    email: 'admin@keystonejs.com',
+    password: 'admin',
+    canAccessKeystone: true,
+  }).save(done);
+};
