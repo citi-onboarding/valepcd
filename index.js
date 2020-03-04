8baf521afd741ae9db48d3131db692aeb4b237ff
Daniel <daniel.pe.15y15@gmail.com>
2020-03-03 21:56:02 -0300

    teste

---
+++ index.js
@@ -0,0 +1,24 @@
+const keystone = require('keystone');
+const path = require('path');
+
+
+require('dotenv').config();
+
+keystone.init({
+  name: 'Vale PCD',
+  static: [path.join(__dirname, './public')],
+  'admin path': 'admin',
+  port: process.env.PORT || 3001,
+  'auto update': true,
+  mongo: process.env.MONGO_URI,
+  auth: true,
+  'user model': 'User',
+  'cookie secret': process.env.COOKIE_SECRET,
+  'cloudinary config': process.env.CLOUDINARY_CONFIG,
+});
+
+keystone.import('./models');
+
+keystone.set('routes', require('./routes'));
+
+keystone.start();
